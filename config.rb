27ae730d6ded1b97c4cee33a30b2fc842da1fc76
Mathieu THOMASSET <mathosset@gmail.com>
2015-06-23 11:35:53 +0200

    Deploy branch gh-pages

--- config.rb
+++ config.rb
@@ -78,7 +78,7 @@ activate :deploy do |deploy|
 
   # Optional Settings
   # deploy.remote = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch = 'custom-branch' # default: gh-pages
+  deploy.branch = 'gh-pages' # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
