b032a7169685c34763eeda90755d058798c90779
Mathieu THOMASSET <mathosset@gmail.com>
2015-06-25 23:41:20 +0200

    Add JS title on projects

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  if ($('.jspane').length) {
+    $('.jspane').each(function() {
+      var $jspane = $(this);
+      $jspane.find('.text').hide();
+      $jspane.hover(function() {
+        $jspane.find('.text').stop(true, true).fadeIn();
+      }, function() {
+        $jspane.find('.text').stop(true, true).fadeOut();
+      });
+
+    });
+  }
+});
