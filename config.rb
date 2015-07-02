c7463d1403d3b544bb920c9ef21be90fca04bca4
Mathieu THOMASSET <mathosset@gmail.com>
2015-07-02 16:11:51 +0200

    Add gem imageoptim

--- config.rb
+++ config.rb
@@ -34,6 +34,8 @@
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
+activate :imageoptim
+
 
 # Reload the browser automatically whenever files change
 configure :development do
