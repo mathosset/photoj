7d7c0f08da776fc86b99668fe363372e5cacdcf3
Mathieu THOMASSET <mathosset@gmail.com>
2015-07-03 12:12:37 +0200

    Add page_image in layout

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -14,30 +14,4 @@ module CustomHelpers
   def current_page_url
     "#{data.site.url}#{current_page.url}"
   end
-
-  def page_url page
-    "#{data.site.url}#{page.url}"
-  end
-
-  def page_image
-    current_page.data.image_path || data.site.logo_image_path
-  end
-
-  # Social share URLs
-  def twitter_url
-    "https://twitter.com/share?text=“#{page_title}”" +
-                               "&url=#{current_page_url}" +
-                               "&via=#{data.site.twitter_handle}"
-  end
-
-  def facebook_url
-    "https://www.facebook.com/dialog/feed?app_id=#{data.site.facebook_app_id}" +
-                                          "&caption=#{page_title}" +
-                                          "&picture=#{page_image}" +
-                                          "&name=“#{page_title}”" +
-                                          "&description=#{page_description}" +
-                                          "&display=popup" +
-                                          "&link=#{current_page_url}" +
-                                          "&redirect_uri=#{current_page_url}"
-  end
 end
