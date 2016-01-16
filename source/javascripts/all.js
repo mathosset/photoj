4a1366c0942758000f5dcdfbd8e89223cf4ee622
Mathieu THOMASSET <mathosset@gmail.com>
2016-01-16 12:13:40 +0100

    Add js grid button

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -17,8 +17,15 @@ $(document).ready(function() {
   }
 });
 
-//= $(document).ready(function() {
-  //= $('#btn-grid').click(function() {
-  //=  $("#grid").removeClass("col-sm-12").addClass("col-sm-4");
-  //= });
-//= });
+$(document).ready(function() {
+  $('#btn-grid').click(function() {
+    $("section div:nth-child(n)").removeClass("col-sm-12").addClass("col-xs-6 col-sm-4");
+    $('#btn-grid').addClass('disabled');
+    $('#btn-list').removeClass('disabled');
+  });
+  $('#btn-list').click(function() {
+    $("section div:nth-child(n)").removeClass("col-xs-6 col-sm-4").addClass("col-sm-12");
+    $('#btn-list').addClass('disabled');
+    $('#btn-grid').removeClass('disabled');
+  });
+});
