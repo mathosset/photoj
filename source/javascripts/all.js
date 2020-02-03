697edaad3b7298f3ec06cbfd378fc0e7a6674dd9
Mathieu Thomasset <mathosset@gmail.com>
2020-02-03 10:06:40 +0100

    Add new contact  form

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -62,3 +62,30 @@ $(document).ready(function() {
       $(".navbar-default .navbar-toggle .icon-bar").css({"background-color":"white"});
     }
 });
+
+$("#contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
