7077ef21f4cac38d9d209d4e4a245019b054524d
Mathieu Thomasset <mathosset@gmail.com>
2020-03-17 18:02:20 +0100

    New tearsheet

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -18,12 +18,12 @@ $(document).ready(function() {
   }
 
   $('#btn-grid').click(function() {
-    $("section div:nth-child(n)").removeClass("col-sm-12").addClass("col-xs-6 col-sm-4");
+    $("section div:nth-child(n)").removeClass("col-sm-12").addClass("col-xs-12 col-sm-4");
     $('#btn-grid').addClass('disabled');
     $('#btn-list').removeClass('disabled');
   });
   $('#btn-list').click(function() {
-    $("section div:nth-child(n)").removeClass("col-xs-6 col-sm-4").addClass("col-sm-12");
+    $("section div:nth-child(n)").removeClass("col-xs-12 col-sm-4").addClass("col-sm-12");
     $('#btn-list').addClass('disabled');
     $('#btn-grid').removeClass('disabled');
   });
