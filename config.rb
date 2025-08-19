fc18ebf46dd782a77e69cecd86e26f9b417cae5f
Mathieu Thomasset <mathosset@gmail.com>
2025-08-19 11:05:17 +0200

    test new deploy method

--- config.rb
+++ config.rb
@@ -136,13 +136,15 @@ configure :build do
 end
 
 # Deployment
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.build_before = true
+# activate :deploy do |deploy|
+#   deploy.deploy_method = :git
+#   deploy.deploy_build_before = true
+  
+#   # deploy.branch = "master"
 
-  # Optional Settings
-  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
+#   # Optional Settings
+#   # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+#   # deploy.branch = 'custom-branch' # default: gh-pages
+#   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+#   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+# end
